846ca9802487602d0b9a227102d8e7e48ab1b16c
Steve Hu <stevehu@gmail.com>
2024-01-28 11:51:48 -0500

    update package

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name="pre_commit_hook_keyword",
     version="1.0.0",
-    packages=["keyword"],
+    packages=["keywordscan"],
     entry_points={"console_scripts": ["keywordscan=keywordscan.cli:main"]},
 )
