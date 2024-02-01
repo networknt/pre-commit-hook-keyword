c04aaf7f076ea4e7185bcb3411bfc90bd6a6931d
Steve Hu <stevehu@gmail.com>
2024-01-31 21:46:13 -0500

    add debug

--- keywordscan/cli.py
+++ keywordscan/cli.py
@@ -33,6 +33,9 @@ def parse_args(argv):
 
 def main(argv=None):
     args = parse_args(argv)
+    
+    print("\n".join(args.keywords))
+
     rc = 0
     for filename in args.filenames:
         calls = find_keywords(filename, args.keywords, ignore=args.ignore)
